6149906ed6e0b6f61b70b4af7301e4ed156f9fe4
andhess <andhess@student.ethz.ch>
2024-09-30 09:30:10 +0200

    initial commit
    Created from https://github.com/caporaso-lab/cookiecutter-qiime2-plugin. See https://develop.qiime2.org to learn more.

---
+++ automated_rarefaction_depth/plugin_setup.py
@@ -0,0 +1,41 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2024, Andrea Hess.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
+from qiime2.plugin import Citations, Plugin
+from q2_types.feature_table import FeatureTable, Frequency
+from automated_rarefaction_depth import __version__
+from automated_rarefaction_depth._methods import duplicate_table
+
+citations = Citations.load("citations.bib", package="automated_rarefaction_depth")
+
+plugin = Plugin(
+    name="rarefaction-depth",
+    version=__version__,
+    website="https://example.com",
+    package="automated_rarefaction_depth",
+    description="This qiime2 plugin gives you a tool to automatically calculate the ideal rarefaction depth based on the given data and some user parameters.",
+    short_description="A tool that automatically calculates the ideal rarefaction depth.",
+    # The plugin-level citation of 'Caporaso-Bolyen-2024' is provided as
+    # an example. You can replace this with citations to other references
+    # in citations.bib.
+    citations=[citations['Caporaso-Bolyen-2024']]
+)
+
+plugin.methods.register_function(
+    function=duplicate_table,
+    inputs={'table': FeatureTable[Frequency]},
+    parameters={},
+    outputs=[('new_table', FeatureTable[Frequency])],
+    input_descriptions={'table': 'The feature table to be duplicated.'},
+    parameter_descriptions={},
+    output_descriptions={'new_table': 'The duplicated feature table.'},
+    name='Duplicate table',
+    description=("Create a copy of a feature table with a new uuid. "
+                 "This is for demonstration purposes only. 🧐"),
+    citations=[]
+)
---
+++ setup.py
@@ -0,0 +1,36 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2024, Andrea Hess.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
+from setuptools import find_packages, setup
+
+import versioneer
+
+description = ("A template QIIME 2 plugin.")
+
+setup(
+    name="automated-rarefaction-depth",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    license="BSD-3-Clause",
+    packages=find_packages(),
+    author="Andrea Hess",
+    author_email="andhess@ethz.ch",
+    description=description,
+    url="https://example.com",
+    entry_points={
+        "qiime2.plugins": [
+            "automated_rarefaction_depth="
+            "automated_rarefaction_depth"
+            ".plugin_setup:plugin"]
+    },
+    package_data={
+        "automated_rarefaction_depth": ["citations.bib"],
+        "automated_rarefaction_depth.tests": ["data/*"],
+    },
+    zip_safe=False,
+)
